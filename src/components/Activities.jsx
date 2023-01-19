f980778e14a9b54083e8319d24309ffb014e036f
mikehwebdev <81847609+mikehwebdev@users.noreply.github.com>
2023-01-19 13:28:01 +0000

    Adjusting import of images

--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -1,5 +1,8 @@
 import '../css/Activities.css'
 import Card from './Card'
+import blakeys from "/src/assets/img/Blakeys.jpg"
+import common from "/src/assets/img/common.jpg"
+import ducks from "/src/assets/img/ducks.jpg"
 
 export default function Activities (){
     
@@ -8,17 +11,17 @@ export default function Activities (){
         <h1>Top three activities to do in Mulbarton</h1>
         <div className="card-container">
         <Card
-                img="/src/assets/img/Blakeys.jpg"
+                img={blakeys}
                 title="Stop at our amazing Bus Café"
                 description="Grab a nibble to eat and a hot cuppa at the amazing converted double decker bus café - Blakeys!"
            />
         <Card
-                img="/src/assets/img/common.jpg"
+                img={common}
                 title="Visit Mulbarton Common"
                 description="Explore the 45 acres of Mulbarton Common with it's many ponds and pits - and watch out for the rabbits!"
            />
          <Card
-                img="/src/assets/img/ducks.jpg"
+                img={ducks}
                 title="Feed the ducks at the pond"
                 description="Help our village keep our Crackers Quackers fat and healthy all year round"
            />   
--- src/components/Passport.jsx
+++ src/components/Passport.jsx
@@ -1,10 +1,11 @@
 import '../css/Passport.css'
+import mike from "/src/assets/img/mike.jpg"
 
 export default function Passport(){
     return (
         <>
         <div className="passport">
-            <img src="/src/assets/img/mike.jpg"/>
+            <img src={mike}/>
             <div className="flex-details">
                 <h1>Your guide</h1>
                 <p>"I've lived in Mulbarton for more than 5 years so I can show you around all the amazing sites and beautiful nature on offer in our village"</p>
